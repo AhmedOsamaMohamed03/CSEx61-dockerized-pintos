86a6a62ed25c4f6c753496d96d54656acae83fa3
AhmediaAshraf <h8590h@gmail.com>
2024-05-01 21:53:44 +0300

    Header Files are Merged

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -5,8 +5,6 @@
 #include <list.h>
 #include <stdint.h>
 
-#include "fixed_point.h"
-
 /* States in a thread's life cycle. */
 enum thread_status
 {
@@ -90,16 +88,13 @@ struct thread
    char name[16];             /* Name (for debugging purposes). */
    uint8_t *stack;            /* Saved stack pointer. */
    int priority;              /* Priority. */
-   int effictivePri;          // For Priority Donations
-   struct list locks;         // For locks the thread holds
-   struct list_elem allelem;  /* List element for all threads list. */
+   int effictivePri;
+   struct lock *waitingOn;
+   struct list locks;
+   struct list_elem allelem; /* List element for all threads list. */
 
-   // added by Hager Melook
-   int nice;
-   real recent_cpu;
    /* Shared between thread.c and synch.c. */
    struct list_elem elem; /* List element. */
-   int64_t wakeup_ticks;  // time to wakeup by ALI HASSAN
 
 #ifdef USERPROG
    /* Owned by userprog/process.c. */
@@ -126,6 +121,7 @@ tid_t thread_create(const char *name, int priority, thread_func *, void *);
 
 void thread_block(void);
 void thread_unblock(struct thread *);
+
 struct thread *thread_current(void);
 tid_t thread_tid(void);
 const char *thread_name(void);
@@ -144,15 +140,5 @@ int thread_get_nice(void);
 void thread_set_nice(int);
 int thread_get_recent_cpu(void);
 int thread_get_load_avg(void);
-
-// added by Hager Melook
-void calculate_priority(struct thread *t,void *aux);
-void calculate_recent_cpu(struct thread *t,void *aux);
-void calculate_recent_cpu_threads(void);
-bool test_not_idle(struct thread *current);
-void calculate_priority_threads(void);
-void calculate_avg_load(void);
-
-bool thread_wakeup_less(struct list_elem *a_,struct list_elem *b_, void *aux); //by ALI HASSAN
 
 #endif /* threads/thread.h */
