38b6231c2aef02348c1e92e3d3e6295cab21c6ca
AhmediaAshraf <h8590h@gmail.com>
2024-05-01 22:49:13 +0300

    Thread.c Merged

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -580,6 +580,9 @@ init_thread(struct thread *t, const char *name, int priority)
   strlcpy(t->name, name, sizeof t->name);
   t->stack = (uint8_t *)t + PGSIZE;
   t->priority = priority;
+  t->effictivePri = priority;
+  list_init(&t->locks);
+  t->waitingOn = NULL;
   t->magic = THREAD_MAGIC;
   // added by Hager Melook
   if (thread_mlfqs)
